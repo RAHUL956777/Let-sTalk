686bff4f1e84d06e094bec7a22c3068c967cb391
RAHUL <dasr16583@gmail.com>
2023-06-01 23:31:26 +0530

    Complete working on draware functionality

--- client/src/components/drawer/Profile.jsx
+++ client/src/components/drawer/Profile.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+import { Box, Typography, styled } from "@mui/material";
+import { useContext } from "react";
+import { AccountContext } from "../../context/AccountProvider";
+
+const ImageContainer = styled(Box)`
+  display: flex;
+  justify-content: center;
+`;
+
+const Image = styled("img")({
+  width: 200,
+  height: 200,
+  borderRadius: "50%",
+  padding: "25px 0",
+});
+
+const BoxWrapper = styled(Box)`
+  background: #FFFFFF;
+  padding: 12px 30px 2px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.08);
+
+  &:first-item {
+    font-size: 13px;
+    color: #009688;
+    font-weight: 200;
+  }
+
+  &:last-child {
+    margin: 14px 0;
+    color: #4a4a4a;
+  }
+`;
+
+const DescriptionContainer = styled(Box)`
+  padding: 15px 20px 28px 30px;
+
+  & > p {
+    font-size: 15px;
+    color: #8696a0;
+  }
+`;
 
 const Profile = () => {
-  return (
-    <></>
-  )
-}
+  const { account } = useContext(AccountContext);
 
-export default Profile
+  return (
+    <>
+      <ImageContainer>
+        <Image src={account.picture} alt="dp" />
+      </ImageContainer>
+
+      <BoxWrapper>
+        <Typography>Your name</Typography>
+        <Typography>{account.name}</Typography>
+      </BoxWrapper>
+
+      <DescriptionContainer>
+        <Typography>
+          This is not your username or pin. This name will be visible to your
+          whatsApp contacts
+        </Typography>
+      </DescriptionContainer>
+
+      <BoxWrapper>
+        <Typography>About</Typography>
+        <Typography>Eat! Sleep! Code! Repeat!</Typography>
+      </BoxWrapper>
+    </>
+  );
+};
+
+export default Profile;
