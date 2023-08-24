24c442d48b3103e4837f36859cddefbe0f7fa154
RAHUL <dasr16583@gmail.com>
2023-08-24 20:39:12 +0530

    working on chat pages component

--- client/src/components/Chatlist/List.jsx
+++ client/src/components/Chatlist/List.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function List() {
-  return <div>List</div>;
+  return (
+    <div className="bg-search-input-container-background flex-auto overflow-auto max-h-full custom-scrollbar"></div>
+  );
 }
 
 export default List;
